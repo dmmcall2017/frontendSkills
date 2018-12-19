8de71ad8a99d15c57db802d4dd9846376907d751
Janice Dong <2430957798@qq.com>
2018-12-19 15:03:13 +0800

    更新：简书、

---
+++ jqApi/js/jqFrame.js
@@ -0,0 +1,8 @@
+(function(global, factory){
+
+})(typeof window !== "undefined" ? window : this, function (window, noGlobal){
+    //变量
+    var arr = [];
+    var document = window.document;
+    var getProto = Object.getPrototypeOf;
+})
--- jqApi/js/jqSimple.js
+++
@@ -1,6 +0,0 @@
-jQuery.fn = jQuery.prototype = {
-    jquery:'版本',
-    constructor: '修正指向问题',
-    init: '初始化函数',
-    selector:'选择器'
-}
