20b83a2a55a7c1653c2854fd5126d3aa6583666e
Janice Dong <2430957798@qq.com>
2019-01-21 09:57:54 +0800

    jq

--- jqApi/js/jqFrame.js
+++ jqApi/js/jqFrame.js
@@ -1,8 +1,22 @@
 (function(global, factory){
 
 })(typeof window !== "undefined" ? window : this, function (window, noGlobal){
-    //变量
+    //-----------变量声明--------------
     var arr = [];
     var document = window.document;
     var getProto = Object.getPrototypeOf;
+
+    //-----------jQuery对象------------
+    var jQuery = function(selector, context){
+        //调用原型方法
+        return new jQuery.fn.init(selector, context);
+    }
+    jQuery.fn = jQuery.prototype = {
+        
+    }
+
+    //----------jQuery扩展函数----------
+    jQuery.extend = jQuery.fn.extend = function(){
+        
+    }
 })
