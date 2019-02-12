e97ece3f97acd455f84f7bccc8eaf5b536afe4b3
Janice Dong <2430957798@qq.com>
2019-02-12 17:17:38 +0800

    ts basic

---
+++ TypeScript/basic1.ts
@@ -0,0 +1,11 @@
+let isDone:boolean = false;
+console.log(isDone);
+let list: number[] = [1,2,3];
+let list1: Array<Object> = [{dmm:'djfhdk'},12,3]
+
+let x: [string,number];
+x = ['hello',10]
+
+//枚举
+enum Color {Green,Blue,Red}
+let c: Color = Color.Green;
